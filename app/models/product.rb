0fe9373cd05407900c144fbbbea6802cafde1c32
koleshkos <koleshkoser@gmail.com>
2022-01-31 23:04:14 +0300

    DP4-2 - Added Bootstrap

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :name, presence: true, length: {minimum: 2, maximun: 500}
-  validates :code, presence: true, format: { with: /^[a-z0-9]+[-a-z0-9]*[a-z0-9]+$/i }, length: { is: 7}
+  validates :code, presence: true, format: { with: /[a-zA-Z0-9]+\z/ }, length: { is: 7 }
   validates :price, numericality: { greater_than: 0, less_than: 1000000 }, length: {minimum: 2, maximun: 500}
   validates :description, length: {minimum: 1, maximun: 5000}
 end
