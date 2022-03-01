60082dbf4447a8d44bf4575a25a53be6191dd421
koleshkos <koleshkoser@gmail.com>
2022-03-01 21:02:39 +0300

    DP4-3 - Resize image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
 
@@ -36,9 +35,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fill: [400, 400]
+  end
+
+  version :big do
+    process resize_to_fill: [1000, 1000]
+  end
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
