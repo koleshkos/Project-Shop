27a5bd891f8528682f22914074cc04655d945f0b
koleshkos <koleshkoser@gmail.com>
2022-05-06 13:01:43 +0300

    Fix import js module for deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,8 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "packs/checkbox"
-import "packs/card"
+import "packs/hover_chechbox"
+import "packs/product_delete"
 require('jquery')
 
 Rails.start()
