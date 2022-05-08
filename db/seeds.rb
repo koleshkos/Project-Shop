2bf356994d8881014d76b42cb3eb7859332357bf
koleshkos <koleshkoser@gmail.com>
2022-05-09 00:22:33 +0300

    Setup seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do |_product|
+  Product.create(
+    name: Faker::Device.model_name,
+    image: Faker::File.file_name(ext: 'jpg'),
+    code: Faker::Lorem.characters(number: 7),
+    price: Faker::Number.number(digits: 7),
+    description: Faker::Lorem.paragraph_by_chars(number: 1000),
+    status: 'active',
+  )
+end
